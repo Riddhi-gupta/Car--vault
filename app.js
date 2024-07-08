489c918e4b66e5c64b51741d0e403e1c8ec4ca3f
Riddhi-gupta <114225420+Riddhi-gupta@users.noreply.github.com>
2024-07-08 20:30:20 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CarList from './components/CarList';
+
+const App = () => {
+  return (
+    <div className="App">
+      <CarList />
+    </div>
+  );
+};
+
+export default App;
